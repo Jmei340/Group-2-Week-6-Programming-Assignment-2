fa7e1368bad6733d881c86672959f5dd6f29c36c
Jmei340 <meij340@macomb.edu>
2023-12-02 17:55:30 -0500

    Added Code To Some Functions

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@
 
 #include <iostream>
 #include <iomanip>
+#include <fstream>
 #include <string>
+#include <algorithm>
 #include <cmath>
 using namespace std;
 // Add any other header files here
@@ -58,49 +60,101 @@ const string GetMostWinningTeam(const string teams[], const int maxArraySize)
 // This function returns the team that won the world series on the "date".
 const string GetTeamForDate(const string date, const string teams[], const string dates[], const int maxArraySize)
 {
-    // Work here.
+    // Declaring Variables
+    int found = 0;
+    int index = 0;
+
+    // Finding Teams From Dates
+    while (found != 1) {
+
+        if (date != dates[index]) {
+            index++;
+        }
+        else {
+            found = 1;
+        }
+    }
+
+    // Outputting Team
+    cout << "The team that won the world series on '" << date << "' is '" << teams[index] << "'.";
+
+    return 0;
 }
 
 // This function changes all of the team names to either upper or lower case depending on the value of "to_upper".
- void ConvertTeamNameCase(bool to_upper, string teams[], const int maxArraySize)
+void ConvertTeamNameCase(bool to_upper, string teams[], const int maxArraySize)
 {
-    // Work here.
+    // Declaring Variables
+    char choice = 'a';
+    int index = 0;
+
+    // Getting User Input
+    cout << "Please enter the case to change ('u' = upper and 'l' = lower):";
+    cin >> choice;
+
+    // Upper or Lower Case
+    if (choice == 'u' || choice == 'U') {
+        for (index = 0; index < maxArraySize; index++) {
+            transform(teams[index].begin(), teams[index].end(), teams[index].begin(), ::toupper);
+        }
+
+        cout << "The case for all team names has been changed to 'upper'.";
+    }
+    else if (choice == 'l' || choice == 'L') {
+        for (index = 0; index < maxArraySize; index++) {
+            transform(teams[index].begin(), teams[index].end(), teams[index].begin(), ::tolower);
+        }
+
+        cout << "The case for all team names has been changed to 'lower'.";
+    }
 }
 
 // This function provides the menu and calls the other functions according to what the user inputs. 
 // See pic in assignment for more details.
 int main(int argc, char* argv[])
 {
-	if (argc < 3)
-	{
-		cout << "Error! Not enough file names. Expecting 2!" << endl;
-		return -1;
-	}
+    if (argc < 3)
+    {
+        cout << "Error! Not enough file names. Expecting 2!" << endl;
+        return -1;
+    }
 
     // Const representing the number of lines of data in both files.
-	const int SIZE_DATA = 120;
+    const int SIZE_DATA = 120;
 
     // Declaring Variables
-    int WinnerDates[SIZE_DATA];
+    string data[SIZE_DATA];
+    string filename;
+    string dates[SIZE_DATA];
+    string date;
     string message;
+    string teamNames;
+    string teams[SIZE_DATA];
+    char c = 'a';
+    bool to_upper = false;
 
-    
+
+
     do {
         // Getting Introduction
-        Introduction(c, message);
+        Introduction(c, message);               // Function works
+        ReadFile(filename, data, SIZE_DATA);    // In Progress
 
         // Calling Functions
         if (c == 'c' || c == 'C') {
-
+            GetCountOfTeam(teamNames, teams, SIZE_DATA);        // In Progress
         }
         else if (c == 'w' || c == 'W') {
-
+            GetMostWinningTeam(teams, SIZE_DATA);               // In Progress
         }
         else if (c == 'd' || c == 'D') {
+            cout << "Please enter the date:";
+            cin >> date;
 
+            GetTeamForDate(date, teams, dates, SIZE_DATA);      // Function needs testing
         }
         else if (c == 'v' || c == 'V') {
-
+            ConvertTeamNameCase(to_upper, teams, SIZE_DATA);    // Function needs testing
         }
 
 
